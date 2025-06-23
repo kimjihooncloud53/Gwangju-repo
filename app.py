883afc766b7ef4a256eeb46612817d91b134f38a
김지훈 <s24021@gsm.hs.kr>
2025-06-23 09:38:02 +0900

    change name

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+TAG = "ec2-v1.0.0"
+
+@app.route("/")
+def home():
+    return f"Hello Gwangju!", 200
+
+@app.route("/health")
+def health():
+    return "OK", 200
+
+@app.route("/tag")
+def tag():
+    return TAG, 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
